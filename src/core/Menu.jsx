38fb9bb8d240787f867308fd9507106c6884b7e4
DariusM6s <dariusm6s@outlook.com>
2020-02-10 13:28:34 +0000

    initial commit moving to windows

---
+++ src/core/Menu.jsx
@@ -0,0 +1,75 @@
+import React, { Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
+
+const isActive = (history, path) => {
+	if (history.location.pathname === path) {
+		return { color: '#ff9900' };
+	}
+	return { color: '#ffffff' };
+};
+
+const Menu = ({ history }) => (
+	<div>
+		<ul className='nav nav-tabs bg-dark nav-fill'>
+			<li className='nav-item'>
+				<Link className='nav-link' style={isActive(history, '/')} to='/'>
+					Home
+				</Link>
+			</li>
+
+			{isAuthenticated() &&
+			isAuthenticated().user.role === 0 && (
+				<li className='nav-item'>
+					<Link
+						className='nav-link'
+						style={isActive(history, '/user/dashboard')}
+						to='/user/dashboard'>
+						Dashboard
+					</Link>
+				</li>
+			)}
+
+			{isAuthenticated() &&
+			isAuthenticated().user.role === 1 && (
+				<li className='nav-item'>
+					<Link
+						className='nav-link'
+						style={isActive(history, '/admin/dashboard')}
+						to='/admin/dashboard'>
+						Dashboard
+					</Link>
+				</li>
+			)}
+
+			{!isAuthenticated() && (
+				<Fragment>
+					<li className='nav-item'>
+						<Link className='nav-link' style={isActive(history, '/signin')} to='/signin'>
+							SignIN
+						</Link>
+					</li>
+
+					<li className='nav-item'>
+						<Link className='nav-link' style={isActive(history, '/signup')} to='/signup'>
+							SignUP
+						</Link>
+					</li>
+				</Fragment>
+			)}
+
+			{isAuthenticated() && (
+				<li className='nav-item'>
+					<span
+						className='nav-link'
+						style={{ cursor: 'pointer', color: '#ffffff' }}
+						onClick={() => signout(() => history.push('/'))}>
+						SignOUT
+					</span>
+				</li>
+			)}
+		</ul>
+	</div>
+);
+
+export default withRouter(Menu);
